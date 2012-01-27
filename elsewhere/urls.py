3371c629aacb496bd614b121cdb9d50fc3c641b6
Brian Neal <bgneal@gmail.com>
2012-01-26 19:49:27 -0600

    Update to use django.contrib.staticfiles app.

--- elsewhere/urls.py
+++ elsewhere/urls.py
@@ -1,20 +1,5 @@
-import settings
-import os
-
-from django.conf.urls.defaults import *
-from django.contrib import admin
-
-admin.autodiscover()
+from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('elsewhere.views',
     (r'^$', 'example'),
 )
-
-if settings.DEBUG:
-    CUR_DIR = os.path.dirname(__file__)
-    IMG_PATH = 'img/'
-    IMG_DIR = os.path.join(CUR_DIR, "img")
-
-    urlpatterns += patterns('django.views',
-        url(r'^%s(?P<path>.*)' % IMG_PATH, 'static.serve', {'document_root': IMG_DIR}, name='elsewhere_img')
-    )
